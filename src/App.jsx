c9de21179d04cf44460420469eaead9d3cad2302
harshshukla9 <harshshukla9792@gmail.com>
2024-09-01 20:19:07 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Button } from './components/ui/button'
-import Navbar from './components/ui/Component/Navbar'
-import Walletsgenerator from './components/ui/Component/Walletsgenerator'
-import Footer from './components/ui/Component/Footer'
-import Layout from './components/ui/Component/Layout'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-   
-    <Layout>
-      <Navbar/>
-      <Walletsgenerator />
-    </Layout>
+   <Button className="bg-slate-600">hello</Button>
 
     
     
